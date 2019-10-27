85bbd241f6601d02ebb7c70097da41a6036b22a9
Karim <karim20.ks@gmail.com>
2019-10-27 17:15:43 +0600

    add models

--- sport_app/urls.py
+++ sport_app/urls.py
@@ -18,5 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('registration', views.RegistrationView.as_view(), name='registration'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('test', views.test, name='test'),
+    path('logout', views.logout1, name='logout')
 ]
