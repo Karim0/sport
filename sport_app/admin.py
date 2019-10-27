fe60324ce8af561684a2b839746c3ffb77ab3e36
Karim <karim20.ks@gmail.com>
2019-10-27 15:46:17 +0600

    add models

--- sport_app/admin.py
+++ sport_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(SportSection)
+admin.site.register(Location)
+admin.site.register(Rating)
+admin.site.register(Review)
--- sport_app/models.py
+++ sport_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,7 +16,25 @@ class SportSection(models.Model):
 class Location(models.Model):
     location = models.TextField()
     map_location = models.TextField()
-    section_id = models.ForeignKey(SportSection)
+    section_id = models.ForeignKey(SportSection, on_delete=models.PROTECT)
 
     def __str__(self):
-        return "location = {0}, map_location = {1}, section_id = {2}".format(self.location, self.map_location, self.section_id.__str__())
+        return "location = {0}, map_location = {1}, section_id = {2}".format(self.location, self.map_location,
+                                                                             '''self.section_id''')
+
+
+class Rating(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    section_id = models.ForeignKey(SportSection, on_delete=models.PROTECT)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return "user_id = {0}, section_id = {1}, rating = {2}".format(self.user_id, self.section_id, self.rating)
+
+
+class Review(models.Model):
+    section_id = models.ForeignKey(SportSection, on_delete=models.PROTECT)
+    review = models.TextField()
+
+    def __str__(self):
+        return "section_id = {0}, review = {1}".format(self.section_id_id, self.review)
