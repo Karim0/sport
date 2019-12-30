3bff034a2f87f0afc54b0476db11b04a10d276b6
Karim <karim20.ks@gmail.com>
2019-12-30 11:55:37 +0600

    1

--- sport/settings.py
+++ sport/settings.py
@@ -141,4 +141,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'home/karim/projects/sport/media'
